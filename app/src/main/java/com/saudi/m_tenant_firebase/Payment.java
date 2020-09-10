e04a87be199592b019ef3e5a38ba23d2e1846f93
Trevor Saudi <isatrevor052@gmail.com>
2020-09-10 13:35:29 +0300

    firebase, mpesa , UI

---
+++ app/src/main/java/com/saudi/m_tenant_firebase/Payment.java
@@ -0,0 +1,14 @@
+package com.saudi.m_tenant_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
